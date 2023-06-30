2f4e455bd15fb6ee3777bc572375007fb479e264
DanielMuringe <danielmuringe@gmail.com>
2023-06-30 21:12:54 +0300

    Squashed 'canvas-learn/' changes from e705472..f561f44
    f561f44 modify: enured balls dont get stuck to screen
    
    git-subtree-dir: canvas-learn
    git-subtree-split: f561f443d5f50e0318c020d13b84fe457c2d1a8e

--- index.js
+++ index.js
@@ -30,92 +30,88 @@ let degreesToRadians = degrees => (2*Math.PI)*(degrees/360)
 
 // Generate random integers between 0 and num
 let randInt = (...bounds) => {
-    if (1>bounds.length>2)
-        throw Error('Only 2 numbers can be passed as arguments, (stop only) or (start and stop)');
-
+    if (1>bounds.length>3)
+        throw Error('A maximum of 3 numbers can be passed as arguments, (stop only) or (start and stop)');
+    
+        
+    let val = 0;
     if (bounds.length==1)
-        return Math.floor(Math.random() * bounds[0]);
-    else
-        return (Math.floor(Math.random() * (bounds[1]-bounds[0]))+bounds[0]);
+        val =  Math.floor(Math.random() * bounds[0]);
+    else if (bounds.length==2)
+        val = (Math.floor(Math.random() * (bounds[1]-bounds[0]))+bounds[0]);
+    else if (bounds.length==3){
+        while (bounds[2](val)){
+            val = (Math.floor(Math.random() * (bounds[1]-bounds[0]))+bounds[0]);
+        }
+    }
+
+    return val;
+        
 };
 
 
 //  Generate list of random integers
 let randList = (len, ...bounds)=> {
     let List = [];
-    for (let index = 0; index < len; index++) List.push(...bounds);
+    for (let index = 0; index < len; index++) List.push(randInt(...bounds));
+    return List;
 }
 
 
 // Random x and y coordinates
-let xRand = (n=1)=>randInt(n,window.innerWidth);
-let yRand = (n=1)=>randInt(n,window.innerHeight);
-let randCoord = (n=1)=>[xRand(n), yRand(n)];
+let xRand = (n)=>randInt(n,window.innerWidth-n);
+let yRand = (n)=>randInt(n,window.innerHeight-n);
+let randCoord = (n)=>[xRand(n), yRand(n)];
 
 
 class Canvas{
-    
-        render(obj=this){
-    
-            
-            function bouncingCircle(radius,centre=randCoord(),velocity, colour='black'){
-                this.radius = radius;
-                this.centre = centre;
-                let [x,y] = this.centre;
-                let [dx,dy] = this.velocity = velocity; 
-    
-                this.draw = ()=>{
-                    // Draw circle on canvas
-                    obj.circle(this.radius, this.centre, colour);
-    
-                    // Create bounding effect of circle
-                    let xImpact = x+this.radius>=window.innerWidth||x-this.radius<=0;
-                    let yImpact = y+this.radius>=window.innerHeight||y-this.radius<=0;
-                    if (xImpact) dx = -dx;
-                    if (yImpact) dy = -dy;
-                    x+=dx; y+=dy;
-                    this.centre[0]=x;this.centre[1]=y;
-                    this.velocity[0]=dx;this.velocity[1]=dy;
-                } 
-            }
-    
-            function bouncingSquare(radius,centre=randCoord(),velocity, colour='black'){
-                this.radius = radius;
-                this.centre = centre;
-                let [x,y] = this.centre;
-                let [dx,dy] = this.velocity = velocity; 
-    
-                this.draw = ()=>{
-                    // Draw circle on canvas
-                    obj.circle(this.radius, this.centre, colour);
-    
-                    // Create bounding effect of circle
-                    let xImpact = x+this.radius>=window.innerWidth||x-this.radius<=0;
-                    let yImpact = y+this.radius>=window.innerHeight||y-this.radius<=0;
-                    if (xImpact) dx = -dx;
-                    if (yImpact) dy = -dy;
-                    x+=dx; y+=dy;
-                    this.centre[0]=x;this.centre[1]=y;
-                    this.velocity[0]=dx;this.velocity[1]=dy;
-                } 
-            }
-    
-            let balls = [];
-            for (let i=0; i<randInt(15,20); i++) {
-                balls.push(new bouncingCircle(
-                                    randInt(5,30),
-                                    randCoord(50),
-                                    [randInt(-5,5),randInt(-5,5)],
-                                    `rgb(${randInt(255)},${randInt(255)},${randInt(255)})`));
-            }
-            function animate(){
-                requestAnimationFrame(animate);
-                obj.ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
-                for (const ball of balls) ball.draw();
-            }
-            animate()
+
+    render(obj=this){
+
+        
+        function bouncingCircle(radius,centre=randCoord(),velocity, colour='black'){
+            this.radius = radius;
+            this.centre = centre;
+            let [x,y] = this.centre;
+            let [dx,dy] = this.velocity = velocity; 
+
+            this.draw = ()=>{
+                // Draw circle on canvas
+                obj.circle(this.radius, this.centre, colour);
+
+                // Create bounding effect of circle
+                let xImpactWall = x+this.radius>=window.innerWidth||x-this.radius<=0;
+                let yImpactWall = y+this.radius>=window.innerHeight||y-this.radius<=0;
+                if (xImpactWall) dx *= -1;
+                if (yImpactWall) dy *= -1;
+                x+=dx; y+=dy;
+                this.centre[0]=x; this.centre[1]=y;
+                this.velocity[0]=dx; this.velocity[1]=dy;
+
+                // console.log('velocity: ',this.velocity, 'centre', this.centre);
+
+                return this.centre;
+            } 
         }
 
+        let balls = [];
+        let ballCount = 10;
+        for (let i=0; i<ballCount; i++) {
+            balls.push(new bouncingCircle(
+                                randInt(30,30),
+                                randCoord(50),
+                                randList(2,-5,5,(val=>val==0)),
+                                `rgb(${randInt(255)},${randInt(255)},${randInt(255)})`));
+        }
+        function animate(){
+            requestAnimationFrame(animate);
+            obj.ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+            for (const ball of balls) ball.draw();
+        }
+        animate()
+    }
+    
+    
     constructor(){
         // Create tag and assign attributes
         this.tag = document.createElement('canvas');
@@ -127,15 +123,20 @@ class Canvas{
         this.defaultColor = '#000000';
 
         // Add object to locate position of mouse
-        this.mouse = {x: NaN, y:NaN}
+        this.mouse = {x: NaN, y:NaN, capture:false}
         window.addEventListener('mousemove',
             (event)=>{
-                this.mouse.x = event.x;
-                this.mouse.y = event.y;;})
+                if (this.mouse.capture){
+                    this.mouse.x = event.x;
+                    this.mouse.y = event.y;
+                }
+            }
+        )
         
         // Render stuff on canvas
         this.render();
     }
+    
 
     tagSetter(tag=this.tag){
 
@@ -186,8 +187,4 @@ class Canvas{
 }
 
 CANVAS = new Canvas();
-
-
-let newList = [1,2,3,4]
-
 
